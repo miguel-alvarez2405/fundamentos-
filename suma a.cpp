d1db54e915a05957a2cdd539d795d61657802a70
miguel-alvarez2405 <69353519+miguel-alvarez2405@users.noreply.github.com>
2020-08-14 09:46:01 -0400

    Add files via upload

---
+++ suma a.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream> 
+
+using namespace std; 
+#include "conio.h"
+
+int main()
+{
+int a,b,c;
+    cout<<"ingresa el primer operando:" <<endl;
+    cin>>a;
+    cout<<"ingresa el segundo operando";
+    cin>>b;
+    c= a+b;
+    cout<<"la suma es:"<<c;
+return 0;
+
+}    
+
+
