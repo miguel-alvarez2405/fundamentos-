231e0b74a9ea8cc89e9a5b1fadbf80b052e064ac
miguel-alvarez2405 <69353519+miguel-alvarez2405@users.noreply.github.com>
2020-08-18 09:54:44 -0400

    Add files via upload

---
+++ calcular los dias de trabajo.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float s,j,d;
+    cout<<"Ingrsar los dias"<<endl;
+    cin>> d;
+    cout<<"el monto del jornal"<<endl;
+    cin>>j;
+    s=d*j;
+    cout<<"el salario es: "<<s;
+    return 0;
+}
