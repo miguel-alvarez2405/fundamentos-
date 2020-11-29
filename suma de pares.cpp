e61e93b1ef44195428a1d7a07470fd4ce7471683
miguel-alvarez2405 <69353519+miguel-alvarez2405@users.noreply.github.com>
2020-11-28 21:16:59 -0400

    Create suma de pares.cpp

---
+++ suma de pares.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "conio.h"
+#include <iostream>
+using namespace std;
+
+void main() 
+{
+     int n=0, suma;
+     suma=0;
+cout<<"Ingresa el valor de n: ";
+cin>>n;
+for (int i=1; i<=n; i++){
+	if(i%2 ==0)
+	suma=suma+i;
+	
+}
+cout<<"la suma de los pares es: "<<suma;
+
+
+getch();
+
+}
